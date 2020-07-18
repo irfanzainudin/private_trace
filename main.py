56376c877073cd7830b75e45a438d02d56bcc9dc
Razin Idzuddin <razin99@gmail.com>
2020-07-18 18:54:36 +1000

    Added svg

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/user")
+def user_login():
+    icon="./static/icon.svg"
+    return render_template("user_login.html",icon=icon)
+
 if __name__ == "__main__":
     app.run(debug=True)
